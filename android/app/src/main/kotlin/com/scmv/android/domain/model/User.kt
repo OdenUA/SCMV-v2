fda2cff93eb85d3df258f27eb7d0823f81bdaa2f
OdenUA <odenpost@gmail.com>
2026-01-28 20:38:40 +0200

    chore: remove nested web/ and android/ folders (cleanup duplicate files)

--- android/app/src/main/kotlin/com/scmv/android/domain/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.scmv.android.domain.model
-
-data class User(
-    val uid: Int,
-    val username: String
-)
--- android/build-debug.bat
+++
@@ -1,3 +0,0 @@
-$env:JAVA_HOME = "C:\Program Files\Java\jdk-21"; 
-$env:ANDROID_HOME = "C:\Users\Izhe\AppData\Local\Android\Sdk"; 
-.\gradlew.bat assembleDebug
